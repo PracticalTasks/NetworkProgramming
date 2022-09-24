3d5f78a7a2066bcd60997ae2be2c6fbaedfa2fee
Alexandr <kajlaleksandr1986@gmail.com>
2022-09-24 10:51:40 +0700

    Написал класс EchoServer

---
+++ NetworkProgramming/EchoServer.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <cassert>
+#include <chrono>
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <thread>
+
+#include <socket_wrapper/socket_headers.h>
+#include <socket_wrapper/socket_wrapper.h>
+#include <socket_wrapper/socket_class.h>
+
+
+class EchoServer
+{
+public:
+	EchoServer(const uint16_t READ_PORT);
+	~EchoServer();
+
+private:
+	inline std::string& rtrim(std::string& s);
+	int listenTo(const uint16_t READ_PORT);
+
+};
--- NetworkProgramming/main.cpp
+++ NetworkProgramming/main.cpp
@@ -1,6 +1,14 @@
-#include "UdpServer.h"
+#include "EchoServer.h"
 
 int main(int argc, char argv[])
 {
+    //if (argc != 2)
+//{
+//    std::cout << "Usage: " << argv[0] << " <port>" << std::endl;
+//    return EXIT_FAILURE;
+//}
 
+    //const int port { std::stoi(argv[1]) };
+    const uint16_t READ_PORT = 5555;
+    EchoServer echo(READ_PORT);
 }
