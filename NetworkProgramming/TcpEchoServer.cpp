fe8e69b713ffb77a7aca04c9a94806afa3de176e
Alexandr <kajlaleksandr1986@gmail.com>
2022-10-06 14:24:34 +0700

    commit

--- NetworkProgramming/TcpEchoServer.cpp
+++ NetworkProgramming/TcpEchoServer.cpp
@@ -3,12 +3,114 @@
 
 TcpEchoServer::TcpEchoServer(const uint16_t READ_PORT)
 {
-    read(READ_PORT);
+    if (!start_server(READ_PORT))
+    {
+        std::cout << "Can not running echo tcp server on the port " << READ_PORT << std::endl;
+        return;
+    }
+       
+    read();
 }
 
 TcpEchoServer::~TcpEchoServer()
 {
+    delete tcp_echosock;
 }
+
+bool TcpEchoServer::start_server(const uint16_t READ_PORT)
+{
+    tcp_echosock = new socket_wrapper::Socket(AF_INET, SOCK_STREAM, NULL);
+
+    //std::cout << "Starting echo server on the port " << READ_PORT << "...\n";
+
+    if (!tcp_echosock)
+    {
+        std::cerr << sock_wrap.get_last_error_string() << std::endl;
+        return false;
+    }
+
+    sockaddr_in addr =
+    {
+        .sin_family = AF_INET,
+        .sin_port = htons(READ_PORT),
+    };
+
+    addr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(*tcp_echosock, reinterpret_cast<const sockaddr*>(&addr), sizeof(addr)) != 0)
+    {
+        std::cerr << sock_wrap.get_last_error_string() << std::endl;
+        // Socket will be closed in the Socket destructor.
+        return false;
+    }
+
+    if (listen(*tcp_echosock, SOMAXCONN) != SOCKET_ERROR)
+    {
+        std::cout << "Running echo tcp server on the port " << READ_PORT << "...\n";
+        return true;
+    }
+        
+    return false;
+}
+
+int TcpEchoServer::read()
+{
+    char client_addrbuf[INET_ADDRSTRLEN];
+
+    sockaddr_in addr_c;
+    int addrlen = sizeof(addr_c);
+    socket_wrapper::Socket client_sock = accept(*tcp_echosock, reinterpret_cast<sockaddr*>(&addr_c), &addrlen);
+
+    if (!client_sock)
+    {
+        std::cerr << sock_wrap.get_last_error_string() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::array <char, BUFF_SIZE> buff{};
+    short packet_size = 0;
+
+    while (true)
+    {
+
+        packet_size = recv(client_sock, buff.data(), buff.size(), 0);
+
+        if (packet_size > 0)
+        {
+            std::cout
+                << "Client with address "
+                << inet_ntop(AF_INET, &addr_c.sin_addr, client_addrbuf, sizeof(client_addrbuf) / sizeof(client_addrbuf[0]))
+                << ":" << ntohs(addr_c.sin_port)
+                << " sent datagram "
+                << "[length = "
+                << packet_size
+                << "]:\n'''\n"
+                << buff.data()
+                << "\n'''"
+                << std::endl;
+
+            if (cmp_chartostr(buff.data(), CMD_EXT, packet_size))
+            {
+                std::cout << "Echo server has been stopped ...\n";
+                break;
+            }
+
+            packet_size = send(client_sock, buff.data(), packet_size, 0);
+
+            if (packet_size == SOCKET_ERROR)
+            {
+                std::cerr << "Can't send message to Client. Error #" << sock_wrap.get_last_error_string() << std::endl;
+                return EXIT_FAILURE;
+            }
+        }
+
+        buff.fill(0);
+        std::cout << std::endl;
+    }
+
+    return EXIT_SUCCESS;
+}
+
 
 //Метод для сравнения символьной строки с string
 inline bool TcpEchoServer::cmp_chartostr(const char* buf, const std::string& cmd, const int lenBuf)
@@ -23,88 +125,4 @@ inline bool TcpEchoServer::cmp_chartostr(const char* buf, const std::string& cmd
     }
 
     return true;
-}
-
-int TcpEchoServer::read(const uint16_t READ_PORT)
-{
-    socket_wrapper::SocketWrapper sock_wrap;
-    socket_wrapper::Socket echo_sock = { AF_INET, SOCK_STREAM, NULL };;
-
-    std::cout << "Starting echo server on the port " << READ_PORT << "...\n";
-
-    if (!echo_sock)
-    {
-        std::cerr << sock_wrap.get_last_error_string() << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    sockaddr_in addr =
-    {
-        .sin_family = AF_INET,
-        .sin_port = htons(READ_PORT),
-    };
-
-    addr.sin_addr.s_addr = INADDR_ANY;
-
-    if (bind(echo_sock, reinterpret_cast<const sockaddr*>(&addr), sizeof(addr)) != 0)
-    {
-        std::cerr << sock_wrap.get_last_error_string() << std::endl;
-        // Socket will be closed in the Socket destructor.
-        return EXIT_FAILURE;
-    }
-
-    //if (listen(echo_sock, SOMAXCONN) != SOCKET_ERROR) {
-    //    std::cout << "Start listenin at port " << ntohs(addr.sin_port) << std::endl;
-    //    //printf("Start listenin at port%u\n", ntohs(addr.sin_port));
-    //}
-
-    char buffer[256]{};
-
-    // socket address used to store client address
-    struct sockaddr_in client_addr = {};
-    socklen_t client_addrlen = sizeof(sockaddr_in);
-    ssize_t recv_len {};
-
-    if (listen(echo_sock, SOMAXCONN) != SOCKET_ERROR)
-        std::cout << "Running echo tcp server...\n" << std::endl;
-        
-    char client_addrbuf[INET_ADDRSTRLEN];
-
-    while (true)
-    {
-        // Read content into buffer from an incoming client.
-        recv_len = recvfrom(echo_sock, buffer, sizeof(buffer) - 1, 0,
-            reinterpret_cast<sockaddr*>(&client_addr),
-            &client_addrlen);
-
-        if (recv_len > 0)
-        {
-            buffer[recv_len] = '\0';
-            std::cout
-                << "Client with address "
-                << inet_ntop(AF_INET, &client_addr.sin_addr, client_addrbuf, sizeof(client_addrbuf) / sizeof(client_addrbuf[0]))
-                << ":" << ntohs(client_addr.sin_port)
-                << " sent datagram "
-                << "[length = "
-                << recv_len
-                << "]:\n'''\n"
-                << buffer
-                << "\n'''"
-                << std::endl;
-
-            if (cmp_chartostr(buffer, CMD_EXT, recv_len))
-            {
-                std::cout << "Stoped echo server ...\n";
-                break;
-            }
-                
-            sendto(echo_sock, buffer, recv_len, 0, reinterpret_cast<const sockaddr*>(&client_addr),
-                client_addrlen);
-        }
-
-        std::cout << std::endl;
-    }
-
-    return EXIT_SUCCESS;
-https://habr.com/ru/post/327574/
 }
--- NetworkProgramming/main.cpp
+++ NetworkProgramming/main.cpp
@@ -2,12 +2,13 @@
 
 int main(int argc, char const *argv[])
 {
-    if (argc != 2)
-{
-    std::cout << "Usage: " << argv[0] << " <port>" << std::endl;
-    return EXIT_FAILURE;
-}
+//    if (argc != 2)
+//{
+//    std::cout << "Usage: " << argv[0] << " <port>" << std::endl;
+//    return EXIT_FAILURE;
+//}
 
-    const int READ_PORT { std::stoi(argv[1]) };
+    //const int READ_PORT { std::stoi(argv[1]) };
+    const int READ_PORT{ 5555 };
     TcpEchoServer echo(READ_PORT);
 }
