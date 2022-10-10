d17d28fb76624bfda632d75e3f15027f48262874
Alexandr <kajlaleksandr1986@gmail.com>
2022-10-10 08:43:38 +0700

    commit

--- NetworkProgramming/main.cpp
+++ NetworkProgramming/main.cpp
@@ -2,12 +2,13 @@
 
 int main(int argc, const char* argv[])
 {
-	if (argc != 2)
-	{
-		std::cout << "Usage: " << argv[0] << " <hostname>" << std::endl;
-		return EXIT_FAILURE;
-	}
+	//if (argc != 2)
+	//{
+	//	std::cout << "Usage: " << argv[0] << " <hostname>" << std::endl;
+	//	return EXIT_FAILURE;
+	//}
 	 
-	const std::string arg = { argv[1] };
+	//const std::string arg = { argv[1] };
+	const std::string arg = "asustuf";
 	DnsResolver resolver(arg);
 }
