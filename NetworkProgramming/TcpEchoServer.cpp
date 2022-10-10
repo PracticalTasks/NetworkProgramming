e7368a6e4f771059445f6040512786b342bd8971
Alexandr <kajlaleksandr1986@gmail.com>
2022-10-10 15:22:25 +0700

    Сделана отправка файла по запросу

--- NetworkProgramming/TcpEchoServer.cpp
+++ NetworkProgramming/TcpEchoServer.cpp
@@ -58,93 +58,66 @@ int TcpEchoServer::read()
 
     sockaddr_in addr_c;
     int addrlen = sizeof(addr_c);
-    socket_wrapper::Socket client_sock = accept(*tcp_echosock, reinterpret_cast<sockaddr*>(&addr_c), &addrlen);
-
-    if (!client_sock)
-    {
-        std::cerr << sock_wrap.get_last_error_string() << std::endl;
-        return EXIT_FAILURE;
-    }
 
     std::array <char, BUFF_SIZE> buff{};
-    short packet_size = 0;
+    uint32_t packet_size = 0;
 
     while (true)
     {
+        socket_wrapper::Socket client_sock = accept(*tcp_echosock, reinterpret_cast<sockaddr*>(&addr_c), &addrlen);
 
-        packet_size = recv(client_sock, buff.data(), buff.size(), 0);
-
-        if (packet_size > 0)
+        if (!client_sock)
         {
-            std::string filename(buff.data());
-            //Должна быть папка "E:\Netwk" в которой сервер будет искать файл 
-            std::ifstream file("E:\\Netwk\\" + filename, std::ifstream::binary);
-
-            if (!file)
-            {
-                std::cout << "Cannot open file " << filename << std::endl;
-                continue;
-            }
-
-            //Размер буфера 50 Мб
-            int cnt = 0x500000;
-            
-            std::vector<char> buff_bin(cnt);
-            //char* buff_bin = new char[cnt];
-            file.read(buff_bin.data(), cnt);
-           
-            packet_size = 0;
-            for (auto& i : buff_bin)
-            {
-                if (i == '\0')
-                    break;
-                ++packet_size;
-            }
-
-            packet_size = send(client_sock, buff_bin.data(), packet_size, 0);
-
-            if (packet_size == SOCKET_ERROR)
-            {
-                std::cerr << "Can't send message to Client. Error #" << sock_wrap.get_last_error_string() << std::endl;
-                return EXIT_FAILURE;
-            }
-
-            //delete [] buff_bin;
-            //std::cout
-            //    << "Client with address "
-            //    << inet_ntop(AF_INET, &addr_c.sin_addr, client_addrbuf, sizeof(client_addrbuf) / sizeof(client_addrbuf[0]))
-            //    << ":" << ntohs(addr_c.sin_port)
-            //    << " sent datagram "
-            //    << "[length = "
-            //    << packet_size
-            //    << "]:\n'''\n"
-            //    << buff.data()
-            //    << "\n'''"
-            //    << std::endl;
-
-
-            //if (cmp_chartostr(buff.data(), CMD_EXT, packet_size))
-            //{
-            //    std::cout << "Echo server has been stopped ...\n";
-            //    break;
-            //}
-
-            //packet_size = send(client_sock, buff.data(), packet_size, 0);
-
-            //if (packet_size == SOCKET_ERROR)
-            //{
-            //    std::cerr << "Can't send message to Client. Error #" << sock_wrap.get_last_error_string() << std::endl;
-            //    return EXIT_FAILURE;
-            //}
+            std::cerr << sock_wrap.get_last_error_string() << std::endl;
+            return EXIT_FAILURE;
         }
 
-        buff.fill(0);
-        std::cout << std::endl;
+
+
+        while (true)
+        {
+            packet_size = recv(client_sock, buff.data(), buff.size(), 0);
+
+            if (packet_size > 0)
+            {
+                std::string filename(buff.data());
+                //Должна быть папка "C:\Netwk" в которой сервер будет искать файл 
+                std::ifstream file("C:\\Netwk\\" + filename, std::ifstream::binary);
+
+                if (!file)
+                {
+                    std::cout << "Cannot open file " << filename << std::endl;
+                    continue;
+                }
+
+                // get length of file:
+                file.seekg(0, file.end);
+                int length = file.tellg();
+                file.seekg(0, file.beg);
+
+                std::vector<char> buff_bin(length);
+                file.read(buff_bin.data(), length);
+
+                packet_size = send(client_sock, buff_bin.data(), length, 0);
+
+                if (packet_size == SOCKET_ERROR)
+                {
+                    std::cerr << "Can't send message to Client. Error #" << sock_wrap.get_last_error_string() << std::endl;
+                    return EXIT_FAILURE;
+                }
+            }
+            else if (packet_size == -1)
+                break;
+
+            buff.fill(0);
+            std::cout << std::endl;
+        }
+
     }
+
 
     return EXIT_SUCCESS;
 }
-
 
 //Метод для сравнения символьной строки с string
 inline bool TcpEchoServer::cmp_chartostr(const char* buf, const std::string& cmd, const int lenBuf)
