381608b1247d6399cc829218bbc846b1e4ff56b8
Alexandr <kajlaleksandr1986@gmail.com>
2022-10-30 18:36:54 +0700

    commit

--- NetworkProgramming/main.cpp
+++ NetworkProgramming/main.cpp
@@ -4,24 +4,13 @@
 
 int main(int argc, char const *argv[])
 {
-//    if (argc != 2)
-//{
-//    std::cout << "Usage: " << argv[0] << " <port>" << std::endl;
-//    return EXIT_FAILURE;
-//}
-//
-//    const int READ_PORT { std::stoi(argv[1]) };
-    const int READ_PORT{ 5555 };
-    FtpServer ftp(READ_PORT);
+    if (argc != 2)
+{
+    std::cout << "Usage: " << argv[0] << " <port>" << std::endl;
+    return EXIT_FAILURE;
+}
 
-    //try
-    //{
-    //    boost::asio::io_context io_context;
-    //    TcpServer server(io_context);
-    //    io_context.run();
-    //}
-    //catch (const std::exception& e)
-    //{
-    //    std::cerr << e.what() << std::endl;
-    //}
+    const int READ_PORT { std::stoi(argv[1]) };
+    //const int READ_PORT{ 5555 };
+    FtpServer ftp(READ_PORT);
 }
